05f7e6493b93f154ea1d9fc057c9ee7c848574a4
Muhammad Syarifudin <muhammad.el.syarifudin@gmail.com>
2024-02-17 06:45:40 +0700

    cobain buat python code

---
+++ src/eduweb/kalkulator.py
@@ -0,0 +1,8 @@
+a = 8
+b = 17
+
+while a < b:
+    print(f"{a} belum boleh pulang")
+    a = a + 1
+
+print(f"{a} akhirnya bisa pulang")
---
+++ src/eduweb/logi_ifelse.py
@@ -0,0 +1,10 @@
+warna_buah = "Abu-abu"
+
+if warna_buah == "hijau":
+    print("Belum matang")
+elif warna_buah == "merah":
+    print("Sudah matang")
+elif warna_buah == "kuning":
+    print("hampir matang")
+else:
+    print("busuk")
